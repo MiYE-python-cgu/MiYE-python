7132f95d8d2d6525bf3432619a79b74c5533cc29
keeling-cgu <john.keeling@cgu.edu>
2015-04-25 23:24:12 -0700

    Update MiYE.py

--- MiYE.py
+++ MiYE.py
@@ -1,4 +1,4 @@
-from datetime import datetime,date
+from datetime import datetime, date, timedelta
 import sys
 import MySQLdb
 import calendar
@@ -12,33 +12,34 @@ conn=MySQLdb.connect(host="localhost", user="root", passwd="MIYE",db="miye_hotel
 userCheck=True
 while userCheck:
     
-    userCheck=input(" Please enter your name with a . between first and last ")
+    userName=input(" Please enter your name with a . between first and last ")
+    admin = -1
 
-    if userCheck=="Jack.Torrance":
-        user="Jack.Torrance"
-        admin="1"
-        userCheck = None
-    elif userCheck=="Wendy.Torrance":
-        user="Wendy.Torrance"
-        admin="2"
-        userCheck = None
-    elif userCheck=="Danny.Torrance":
-        user="Danny.Torrance"
-        admin="3"
-        userCheck = None
-    elif userCheck=="Dick.Hallorann":
-        user="Dick.Hallorann"
-        admin="4"
-        userCheck = None
-    elif userCheck=="Delbert.Grady":
-        user="Delbert.Grady"
-        admin="5"
-        userCheck = None
-    elif userCheck=="y.y":
-        user="y.y"
-        admin="6"
-        userCheck = None
-        
+    try:
+           cursor0=conn.cursor()
+           cursor0.execute ("""
+           SELECT AdminID FROM t_admin
+           WHERE t_admin.UserName = '%s';
+           """ % userName)
+
+           while(1):
+               row=cursor0.fetchone()
+               if row==None:
+                   break
+
+               admin= row[0]
+
+               if admin >= 0:
+                   userCheck = None
+
+               else:
+                   userCheck = True
+
+    except MySQLdb.Error:
+           print ("Error connecting to MySQL")
+           sys.exit(1)
+    cursor0.close()
+
 ##Main Menu
 ans=True
 while ans:
@@ -57,18 +58,28 @@ while ans:
       resCheckInDate=input("What day will you be checking in? MM-DD-YY " )
 
       formatter_string="%m-%d-%y"
+
       datetimeIN_object = datetime.strptime(resCheckInDate, formatter_string)
       dateIN_object = datetimeIN_object.date()
 
       print(dateIN_object)
-      print("The day of the week is " + calander.weekday(dateIN_object))
-      
+      print("The day of the week is " + str(calendar.weekday(dateIN_object.year, dateIN_object.month, dateIN_object.day)))
+
       resCheckOutDate=input("What day will you be checking out? MM-DD-YY ")
 
       datetimeOUT_object = datetime.strptime(resCheckOutDate, formatter_string)
-      dateOUT_object = datetimeIN_object.date()
+      dateOUT_object = datetimeOUT_object.date()
       
 ##      print(dateOUT_object)
+      day_count = (datetimeOUT_object - datetimeIN_object).days + 1
+      for single_date in (datetimeIN_object + timedelta(n) for n in range(day_count)):
+        print(single_date.strftime("%Y-%m-%d"))
+
+#      d = dateIN_object.
+#      delta = datetime.
+#      while d <= dateOUT_object:
+#          print (d.strftime("%Y-%m-%d"))
+#          d += 1
 
 ## Find the number of vacancies for each type of room
       single_vac = 0
@@ -81,6 +92,7 @@ while ans:
            SELECT RoomID, RoomType FROM t_room
            WHERE NOT EXISTS (SELECT RoomID FROM t_roomguest
            WHERE t_room.RoomID=t_roomguest.RoomID
+           AND t_roomguest.Canceled = 0
            AND(t_roomguest.CheckIn < %s AND t_roomguest.CheckOut > %s));
            """, (datetimeOUT_object, datetimeIN_object ))
 
@@ -124,7 +136,8 @@ while ans:
               break
           else:
               roomCount=int(input("How many would you like to reserve? "))
-              roomCost=float(input("What was charged per room? "))
+##            roomCost=float(input("What was charged per room? "))
+##              if roomCap == 1 AND calendar.weekday(dateIN_object.year, dateIN_object.month, dateIN_object.day) >= 6 AND  dateIN_object.month, dateIN_object.day
               count1 = 0
               stop1 = 1
 
@@ -136,7 +149,7 @@ while ans:
                       try:
                            cursor2=conn.cursor()
                            cursor2.execute ("""
-                           SELECT RoomID FROM t_room
+                           SELECT RoomID, WkDayOffPrice, WkEndOffPrice, WkDayOnPrice, WkEndOnPrice FROM t_room
                            WHERE t_room.MaxGuestAllowed = %s
                            AND NOT EXISTS (SELECT RoomID FROM t_roomguest
                            WHERE t_room.RoomID=t_roomguest.RoomID
@@ -153,6 +166,71 @@ while ans:
                                    break
                            
                                else:
+##Determine total price for rooms
+                                   roomCost=float = 0
+                                   day_count = (datetimeOUT_object - datetimeIN_object).days
+                                   for single_date in (datetimeIN_object + timedelta(n) for (n) in range(day_count)):
+
+                                       if (calendar.weekday(single_date.year, single_date.month, single_date.day)) <= 4:
+
+                                           if ((int(single_date.strftime("%m")) > 1) & (int(single_date.strftime("%m")) < 5)):
+                                               print('Off Season Weekday')
+                                               roomCost += row[1]
+
+                                           elif ((int(single_date.strftime("%m")) > 8) & (int(single_date.strftime("%m")) < 12)):
+                                               print('Off Season Weekday')
+                                               roomCost += row[1]
+
+                                           elif ((int(single_date.strftime("%m")) == 1) & (int(single_date.strftime("%d")) > 15)):
+                                               print('Off Season Weekday')
+                                               roomCost += row[1]
+
+                                           elif ((int(single_date.strftime("%m")) == 12) & (int(single_date.strftime("%d")) < 15)):
+                                               print('Off Season Weekday')
+                                               roomCost += row[1]
+
+                                           elif ((int(single_date.strftime("%m")) == 5) & (int(single_date.strftime("%d")) < 15)):
+                                               print('Off Season Weekday')
+                                               roomCost += row[1]
+
+                                           elif ((int(single_date.strftime("%m")) == 8) & (int(single_date.strftime("%d")) > 15)):
+                                               print('Off Season Weekday')
+                                               roomCost += row[1]
+
+                                           else:
+                                               print('In Season Weekday')
+                                               roomCost += row[3]
+
+                                       else:
+
+                                           if ((int(single_date.strftime("%m")) > 1) & (int(single_date.strftime("%m")) < 5)):
+                                               print('Off Season Weekend')
+                                               roomCost += row[2]
+
+                                           elif ((int(single_date.strftime("%m")) > 8) & (int(single_date.strftime("%m")) < 12)):
+                                               print('Off Season Weekend')
+                                               roomCost += row[2]
+
+                                           elif ((int(single_date.strftime("%m")) == 1) & (int(single_date.strftime("%d")) > 15)):
+                                               print('Off Season Weekend')
+                                               roomCost += row[2]
+
+                                           elif ((int(single_date.strftime("%m")) == 12) & (int(single_date.strftime("%d")) < 15)):
+                                               print('Off Season Weekend')
+                                               roomCost += row[2]
+
+                                           elif ((int(single_date.strftime("%m")) == 5) & (int(single_date.strftime("%d")) < 15)):
+                                               print('Off Season Weekend')
+                                               roomCost += row[2]
+
+                                           elif ((int(single_date.strftime("%m")) == 8) & (int(single_date.strftime("%d")) > 15)):
+                                               print('Off Season Weekend')
+                                               roomCost += row[2]
+
+                                           else:
+                                               print('In Season Weekend')
+                                               roomCost += row[4]
+
 ##Insert reservation into t_roomguest for the selected number of rooms                                   
                                    try:
                                         cursor3=conn.cursor()
@@ -186,14 +264,25 @@ while ans:
       dateIN_object = datetimeIN_object.date()
       print(dateIN_object)
       cursor=conn.cursor()
-      cursor.execute ("SELECT RoomID FROM t_roomguest WHERE RoomID = %s AND CheckIn = %s", (roomNum, dateIN_object))
+      cursor.execute ("SELECT RoomID, RoomInvoice FROM t_roomguest WHERE Canceled = 0 AND RoomID = %s AND CheckIn = %s", (roomNum, dateIN_object))
       row=cursor.fetchone()
       if row==None:
            print ("No reservation found with that information")
 
       else:
-           cursor.execute ("DELETE FROM t_roomguest WHERE RoomID = %s AND CheckIn = %s", (roomNum, dateIN_object))
-           print ("Reservation deleted")
+           today = datetime.today()
+           refundMult = float = 0
+           if (datetimeIN_object - today).days > 21:
+               refundMult = 1
+           elif (datetimeIN_object - today).days > 3:
+               refundMult = .75
+           else:
+               refundMult = 0
+
+           refund = row[1] * refundMult
+           cursor.execute ("UPDATE t_roomguest SET RoomRefund = %s, Canceled = 1 WHERE Canceled = 0 AND RoomID = %s AND CheckIn = %s", (refund, roomNum, dateIN_object))
+           print ("Reservation canceled. Refund is ", refund)
+
       cursor.close()
       conn.commit()
 
@@ -211,3 +300,4 @@ while ans:
        print("\n Not valid choice try again")
 
        
+    
